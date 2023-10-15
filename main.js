03f8b3c81127babf25ae35a5d24aa4f23ef59b76
Shresth Sharma <101468675+Shresth-Sharma-2008@users.noreply.github.com>
2023-10-15 13:20:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+database = firebase.database()
+function sendNotification(title, options){
+    if ("Notification" in window) {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+          var notification = new Notification(title, options);
+          notification.onclick = function () {
+            window.open(options.data.url);
+          };
+          console.log('ok')
+        } else if (permission === "denied") {
+          console.log("Permission for notifications is denied.");
+        } else {
+          console.log("Permission for notifications is pending.");
+        }
+      });
+    } else {
+      console.log("Notifications are not supported in this browser.");
+    }
+}
+var notificationTitle = "New Message";
+var notificationOptions = {
+    body: "You have received a new message from a friend."
+};
+// sendNotification(notificationTitle, notificationOptions);  
+function get(){
+    var gameStateRef  = database.ref('copier1');
+        gameStateRef.on("value",function(data){
+        var data1 = data.val();
+        console.log(data1)
+        if(data1.this != undefined){
+            console.log('hi');
+        }
+    });
+}
+// get();
